7abdcc56bdd3ee00da0458d2723e8fa157863ef8
brizzl98 <novikovartyom2107@gmail.com>
2023-02-10 16:19:10 +0400

    Created login test

---
+++ tests/Feature/LoginMethodTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+
+class LoginMethodTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('passport:install', ['--force' => true]);
+
+    }
+    /**
+     * Test the login method
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        // Create a user
+        $user = User::factory()->create();
+
+        // Send a login request with the user's email and password
+        $response = $this->json('POST', 'api/login', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        // Assert the status code is 200
+        $response->assertStatus(200);
+
+        // Assert the response contains the token
+        $response->assertJsonStructure([
+            'token'
+        ]);
+    }
+}
