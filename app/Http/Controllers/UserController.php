99cb4a03e1dde33bf0f0bc21c7ca63ed5abda9ed
brizzl98 <novikovartyom2107@gmail.com>
2023-02-13 16:25:32 +0400

    created reset_password method in user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\ResetPasswordRequest;
+use App\Models\ResetPassword;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Services\UserService;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Contracts\Auth\Authenticatable;
 
 
 class UserController extends Controller
@@ -40,6 +44,32 @@ class UserController extends Controller
         $token = $user->createToken('Token Name')->accessToken;
 
         return response()->json(['token' => $token], 200);
+    }
+
+    public function resetPassword(ResetPasswordRequest $request)
+    {
+        $email = $request->input('email');
+        $user_id = auth()->user()->id;
+
+
+        // Generate reset token
+        $resetToken = Str::random(60);
+
+        // Store reset token in ResetPassword table
+        ResetPassword::create([
+            'email' => $email,
+            'token' => $resetToken,
+            'user_id' => $user_id,
+        ]);
+
+        // Send email to the user with reset token
+        $mailData = [
+            'resetToken' => $resetToken,
+        ];
+
+        Mail::to($email)->send(new ResetPasswordMail($mailData));
+
+        return response()->json(['message' => 'Reset password email sent']);
     }
 }
 
