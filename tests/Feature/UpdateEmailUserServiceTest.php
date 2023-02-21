26d5b075fb068ba28635977d34fcd1d9f30607e0
brizzl98 <novikovartyom2107@gmail.com>
2023-02-21 11:49:23 +0400

    finished tests for update users email feature

--- tests/Feature/UpdateEmailUserServiceTest.php
+++ tests/Feature/UpdateEmailUserServiceTest.php
@@ -25,43 +25,22 @@ class UpdateEmailUserServiceTest extends TestCase
     {
         $user = User::factory()->create();
         $this->actingAs($user);
-        $newEmail = 'new_email@example.com';
-        var_dump($user = Auth::user());
+        $newEmail = 'newtestemail@example.com';
         $service = new UpdateEmailUserService();
         $service->updateUserEmail($newEmail);
-
         $this->assertDatabaseHas('users', [
             'email' => $newEmail
         ]);
     }
 
     /** @test */
-    public function newCurrentEmailComparison()
+    public function newToCurrentEmailComparison()
     {
         $user = User::factory()->create();
+        $this->actingAs($user);
+        $email = $user->email;
         $service = new UpdateEmailUserService();
-        $result = $service->updateUserEmail($user->email);
-
+        $result = $service->updateUserEmail($email);
         $this->assertEquals("New and current emails are the same. No need to update", $result);
-    }
-
-    /** @test */
-    public function updateAnotherUsersEmail()
-    {
-        // Create two users
-        $user1 = User::factory()->create();
-        $user2 = User::factory()->create();
-
-        // Login as user1
-        $this->actingAs($user1);
-
-        // Try to update user2's email
-        $response = $this->post('/api/user/update', ['email' => $user2->email]);
-
-        // Check that the response status is 403 (Forbidden)
-        $response->assertStatus(403);
-
-        // Check that the email was not updated
-        $this->assertNotEquals($user2->email, $user2->fresh()->email);
     }
 }
